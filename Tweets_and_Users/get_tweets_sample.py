b6332e4f807fd4e216d7ad97b640431282ab6406
Jessica Garson <jgarson@twitter.com>
2019-06-10 20:34:33 -0400

    updating code style

--- Tweets_and_Users/get_tweets_sample.py
+++ Tweets_and_Users/get_tweets_sample.py
@@ -11,12 +11,12 @@ oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 fetch_response = oauth.fetch_request_token(request_token_url)
 resource_owner_key = fetch_response.get("oauth_token")
 resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
+print("Got OAuth token: {}".format(resource_owner_key))
 
 # Get authorization
 base_authorization_url = "https://api.twitter.com/oauth/authorize"
 authorization_url = oauth.authorization_url(base_authorization_url)
-print("Please go here and authorize: %s" % authorization_url)
+print("Please go here and authorize: {}".format(authorization_url))
 verifier = input("Paste the PIN here: ")
 
 # Get the access token
