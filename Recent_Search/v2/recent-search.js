ccdff63535c7ea13b06e5a8659f8629f4e06eb4c
Chloe Kaliman <ckaliman@twitter.com>
2020-02-20 09:14:30 -0500

    add updated node, ruby, python, java samples to recent search v2

---
+++ Recent_Search/v2/recent-search.js
@@ -0,0 +1,73 @@
+const https = require('https');
+const request = require('request');
+const util = require('util');
+
+const get = util.promisify(request.get);
+const post = util.promisify(request.post);
+
+const consumer_key = ''; // Add your API key here
+const consumer_secret = ''; // Add your API secret key here
+
+const bearerTokenURL = new URL('https://api.twitter.com/oauth2/token');
+const searchURL = new URL('https://api.twitter.com/labs/2/tweets/search');
+
+async function bearerToken (auth) {
+  const requestConfig = {
+    url: bearerTokenURL,
+    auth: {
+      user: consumer_key,
+      pass: consumer_secret,
+    },
+    form: {
+      grant_type: 'client_credentials',
+    },
+  };
+
+  const response = await post(requestConfig);
+  return JSON.parse(response.body).access_token;
+}
+
+(async () => {
+  let token;
+  const query = 'from:twitterdev has:media';
+  const maxResults = 10;
+
+  try {
+    // Exchange your credentials for a Bearer token
+    token = await bearerToken({consumer_key, consumer_secret});
+  } catch (e) {
+    console.error(`Could not generate a Bearer token. Please check that your credentials are correct and that the Filtered Stream preview is enabled in your Labs dashboard. (${e})`);
+    process.exit(-1);
+  }
+
+  const requestConfig = {
+    url: searchURL,
+    qs: {
+      query: query,
+      max_results: maxResults,
+      format: 'compact',
+    },
+    auth: {
+      bearer: token,
+    },
+    headers: {
+      'User-Agent': 'LabsRecentSearchQuickStartJS',
+    },
+    json: true,
+  };
+
+  try {
+    const res = await get(requestConfig);  
+    console.log(res.statusCode);
+    console.log(res);
+    if (res.statusCode !== 200) {
+      throw new Error(res.json);
+      return;
+    }
+
+    console.log(res.json);
+  } catch (e) {
+    console.error(`Could not get search results. An error occurred: ${e}`);
+    process.exit(-1);
+  }
+})();
