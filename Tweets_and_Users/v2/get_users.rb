8f2f0d057221d9911f9b4c917ccfc242f2129518
Chloe Kaliman <ckaliman@twitter.com>
2020-02-20 09:09:33 -0500

    add updated v2 get users code

---
+++ Tweets_and_Users/v2/get_users.rb
@@ -0,0 +1,42 @@
+require 'oauth'
+require 'yaml'
+require 'typhoeus'
+require 'oauth/request_proxy/typhoeus_request'
+
+@consumer_key = # Add your API key here
+@consumer_secret = # Add your API secret key here
+
+@consumer = OAuth::Consumer.new(@consumer_key, @consumer_secret,
+                                :site => 'https://api.twitter.com',
+                                :authorize_path => '/oauth/authenticate',
+                                :debug_output => false)
+
+@request_token = @consumer.get_request_token()
+
+@token = @request_token.token
+@token_secret = @request_token.secret
+puts "Authorize via this URL: #{@request_token.authorize_url()}"
+puts "Enter PIN: "
+@pin = gets.strip
+
+@hash = { :oauth_token => @token, :oauth_token_secret => @token_secret}
+@request_token  = OAuth::RequestToken.from_hash(@consumer, @hash)
+@access_token = @request_token.get_access_token({:oauth_verifier => @pin})
+
+puts "Looking up users with new access token"
+
+@uri = "https://api.twitter.com/labs/2/users/by/username/TwitterDev"
+@options = {
+    :method => :get
+}
+
+@oauth_params = {:consumer => @consumer, :token => @access_token}
+@hydra = Typhoeus::Hydra.new
+@req = Typhoeus::Request.new(@uri, @options) # :method needs to be specified in options
+@oauth_helper = OAuth::Client::Helper.new(@req, @oauth_params.merge(:request_uri => @uri))
+@req.options[:headers].merge!({"Authorization" => @oauth_helper.header}) # Signs the request
+@hydra.queue(@req)
+@hydra.run
+@response = @req.response
+
+puts @response.body
